a1c4e9d40ad0f7a0a5d94584d064becc23a8831f
asanCar <a.sanchez3329@gmail.com>
2025-11-05 07:51:21 +0100

    Refactor Helm values template files

--- terraform/app/data.tf
+++ terraform/app/data.tf
@@ -7,29 +7,3 @@ data "terraform_remote_state" "base" {
 }
 
 data "google_client_config" "default" {}
-
-data "template_file" "primary_values" {
-  template = file("${path.module}/values.yaml.tftpl")
-
-  vars = {
-    app_image = var.app_image
-    app_name = var.app_name
-    app_hostname = var.app_hostname
-    static_ip_name = google_compute_address.primary.name
-    min_replicas = 3
-    max_replicas = 6
-  }
-}
-
-data "template_file" "secondary_values" {
-  template = file("${path.module}/values.yaml.tftpl")
-
-  vars = {
-    app_image = var.app_image
-    app_name = var.app_name
-    app_hostname = var.app_hostname
-    static_ip_name = google_compute_address.secondary.name
-    min_replicas = 1
-    max_replicas = 6
-  }
-}
--- terraform/app/main.tf
+++ terraform/app/main.tf
@@ -7,7 +7,15 @@ resource "helm_release" "primary_app" {
   chart = var.helm_chart_path
 
   values = [
-    data.template_file.primary_values.rendered
+    templatefile("${path.module}/values.yaml.tftpl", {
+        app_image = var.app_image
+        app_name = var.app_name
+        app_hostname = var.app_hostname
+        static_ip_name = google_compute_address.primary.name
+        min_replicas = 3
+        max_replicas = 6
+      }
+    )
   ]
 }
 
@@ -20,6 +28,14 @@ resource "helm_release" "secondary_app" {
   chart = var.helm_chart_path
 
   values = [
-    data.template_file.secondary_values.rendered
+    templatefile("${path.module}/values.yaml.tftpl", {
+        app_image = var.app_image
+        app_name = var.app_name
+        app_hostname = var.app_hostname
+        static_ip_name = google_compute_address.secondary.name
+        min_replicas = 1
+        max_replicas = 6
+      }
+    )
   ]
 }
